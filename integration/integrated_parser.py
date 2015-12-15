97efa5335b8c04155918f5c94f2f290e6fc43a99
Niel Mishra <nielmishra@gmail.com>
2015-12-15 21:52:26 +0530

    multilple with directory

--- integration/integrated_parser.py
+++ integration/integrated_parser.py
@@ -1,23 +1,31 @@
 import os
 import sys
 import numpy as np
+
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 
 def get_plot_files(file):
     open_file = open(os.path.realpath(file),'r')
     raw_data = open_file.read()
     array = raw_data.split('* ')
-
+    
     array.pop(0)
     dict_data = {}
     parameters = []
     parsed_files = []
+    dir1 = str(file)
+        
+    os.mkdir(dir1[:-4])
     for i in range(len(array)):
         dict_data[i] = array[i]
         
         sample = dict_data[i].split('\n')
-        write_file = open('parsed_%s.txt'%(i),'w')
-        
+        write_file = open(os.path.realpath(dir1[:-4])+'/parsed_%s.txt'%(i),'w')
+      
         for j in sample[3:]:
             if j.startswith('Index'):
                 items = j.split()
@@ -28,12 +36,10 @@ def get_plot_files(file):
             else:   
                 write_file.write(j+'\n')
 
-        parsed_files.append('parsed_%s.txt'%(i))
+        parsed_files.append(os.path.realpath(dir1[:-4])+'/parsed_%s.txt'%(i))
     return parameters, parsed_files    
 
-def main():
-
-    file = sys.argv[1]
+def loudew(file):
     parameters, parsed_files = get_plot_files(file)  
 
     for plot_file, parameter in zip(parsed_files, parameters):
@@ -49,6 +55,13 @@ def main():
                 plt.xlabel(parameter[1]), plt.ylabel('missing y label')
                 plt.title('%s  vs  missing y label'%(parameter[1], ))
                 plt.savefig(plot_file+str(i)+'.png'), plt.clf()
-                
+          
+
+def main():
+
+    files = sys.argv[1:]
+    for fi in files:
+        loudew(fi)
+              
 if __name__ == '__main__':
     main()
